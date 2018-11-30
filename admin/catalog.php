2bc87f2adb7ab190a36198e44fd5043eebd13776
Messias <MessiasP>
2018-11-30 01:44:39 -0200

    Ãjustes finais

--- admin/catalog.php
+++ admin/catalog.php
@@ -12,55 +12,71 @@ $op = $_GET["op"];
 
 switch ($op) {
 	case "create":
-	echo ("create");
 	create($mysqli, $_GET["nome"], $_GET["estoque"], $_GET["preco"]);
 	break;
 	case "findAll":
-		echo ("findAll");
         findAll($mysqli);
         break;
 	case "update":
-		echo ("update");
         update($mysqli, $_GET["id"], $_GET["nome"], $_GET["estoque"], $_GET["preco"]);
         break;
 	case "delete":
-	echo ("delete");
 	delete($mysqli, $_GET["id"]);
 		break;
 	default:
-		echo "Operacao invalida";
+		echo "Operacao invalida!";
 		break;
 	}
 	
 	function create($mysqli, $nome, $estoque, $preco) {
+
 		$query = "INSERT INTO produto (nome, estoque, preco) VALUES ('".$nome."', '".$estoque."', '".$preco."')";
 		$result = $mysqli -> query($query);
-		echo ("Produto creado com sucesso!");
+		echo ("Produto criado com sucesso!");
+		echo("\nNome: ");
+		echo($nome);
+		echo(", Quantidade: ");
+		echo($estoque);
+		echo(", Preço: ");
+		echo($preco);
+		echo(".");
 	}
 
 	function findAll($mysqli) {
-		$query = "SELECT nome, estoque, preco FROM produto";
+		$query = "SELECT produto_id, nome, estoque, preco FROM produto";
 		$result = $mysqli->query($query);
 		
 		$dbdata = array();
 	
-		while( $row = $result->fetch_assoc()) {
-			$dbdata[]=$row;
+		while( $row = $result -> fetch_assoc()) {
+			$dbdata[] = $row;
 		}
 		echo json_encode($dbdata);
-		echo ("Produtos pesquisados com sucesso!");
 	}
 
 	function update($mysqli, $id, $nome, $estoque, $preco) {
 		$query = "UPDATE produto SET nome='".$nome."', estoque='".$estoque."', preco='".$preco."' WHERE produto_id='".$id."'";
 		$result = $mysqli->query($query);
 		echo ("Produto atualizado com sucesso!");
+		echo("\nCodígo: ");
+		echo($id);
+		echo(", Nome: ");
+		echo($nome);
+		echo(", Quantidade: ");
+		echo($estoque);
+		echo(", Preço: ");
+		echo($preco);
+		echo(".");
 	}
 
 	function delete($mysqli, $id) {
 		$query = "DELETE FROM produto WHERE produto_id='".$id."'";
 		$result = $mysqli->query($query);
+		
 		echo ("Produto deletado com sucesso!");
+		echo("\nCodígo: ");
+		echo($id);
+		echo(".");
 	}
 
 ?>
--- admin/index.php
+++ admin/index.php
@@ -3,15 +3,11 @@
 <head>
 	<?php require "../config.php"; ?>
 	<meta charset="UTF-8"/>
-	<!--esse abaixo jquery que foi baixado-->
-	<script src="./jquery.js"></script>
 
-	<!-- dropdown dependencies-->
- 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	 <!--fim-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
 		
 	<script>
 		function enviar(){
@@ -28,26 +24,19 @@
 																	"&estoque="+estoque+
 																	"&preco="+preco,
 					function(data){
-						// if(status.value == 'sucess') {
-						alert("\n"+data+ "\nStatus: "+status);
-						this.products = data;
-						// }
-						// alert("Erro, Tente novamente!");
+						alert(data);
+						location.reload();
 				});
 			}
 			
 			if(op.value == 'findAll') {
-
-				var products = [];
+				
 				op = document.getElementById("op").value;
 				
-				$.get("http://localhost/projetoFinal/admin/catalog.php?op="+op,
-					function(data){
-						if(status == 'sucess') {
-						alert("\n"+data+ "\nStatus: "+status);
-						this.products = data;
-						}
-						alert("Erro, Tente novamente!");
+				$.get("http://localhost/projetoFinal/admin/catalog.php?op="+op,function(data){			
+
+					let busca = document.getElementById("repo-list").innerHTML = data;
+
 				});
 			}
 			
@@ -62,7 +51,8 @@
 				$.get("http://localhost/projetoFinal/admin/catalog.php?op="+op+"&id="+id+"&nome="+nome+
 																	  "&estoque="+estoque+"&preco="+preco,
 					function(data){
-						alert("\n"+data+ "\nStatus: "+status);
+						alert(data);
+						location.reload();
 				});
 			}
 
@@ -73,7 +63,8 @@
 				
 				$.get("http://localhost/projetoFinal/admin/catalog.php?op="+op+"&id="+id,
 					function(data){
-						alert("\n"+data+ "\nStatus: "+status);
+						alert(data);
+						location.reload();
 				});
 			}
 
@@ -81,51 +72,73 @@
 	</script>
 		
 </head>
-<body>
-	
-	<div class="container">
+<body style="background-color: black;">
+		<div class="container my-5" style="width: 70rem; heigth: 30rem; background-color: white; padding: 5vh; margin: 3% auto 3% auto;" >
 
-			<h2>Bem Vindo!!</h2>
-			<br>
-			<label for="op">Selecione o metodo que deseja:</label>
-			<select class="form-control" id="op">
-				<option value="create">Cadastrar</option>
-				<option value="update">Atualizar</option>
-				<option value="delete">Deletar</option>
-				<option value="findAll">Ver</option>
-			</select>
-			<br>
-			<!-- <input type = "submit" onclick="enviar()" value="Buscar"> -->
-			<br><br>
-			<h4>Atualizar ou deletar produto:</h4>
-			<label>Codigo desejado:</label>
-			<br>
-			<input type="text" id="id">
-			<br><br>	
-			<h4 style="text-aling: center;">Caso escolha cadastro ou atualização de produto complete os campos abaixo:</h4>	
+				<h2>Bem Vindo!!</h2>
+				<br>
+				<label for="op">Selecione o metodo que deseja:</label>
+				<select class="form-control" id="op">
+					<option value="create">Cadastrar</option>
+					<option value="update">Atualizar</option>
+					<option value="delete">Deletar</option>
+					<option value="findAll">Ver</option>
+				</select>
+				<br><br>
 
-			<label>Nome desejado:</label>
-			<br>
-			<input type="text" id="nome">
-			<br><br>
-			<label>Quantidade em estoque:</label>
-			<br>
-			<input type="text" id="estoque">
-			<br><br>
-			<label>Preço:</label>
-			<br>
-			<input type="text" id="preco">
-			<br>
-			<br>
-			<input type = "submit" onclick="enviar()">
+				<h4>Atualizar ou deletar produto:</h4>
+				<br>
+				<div class="form-group">
+					<input type="text" id="id" class="form-control" required>
+					<label class="form-control-placeholder">Codigo desejado:</label>
+				</div>
+				<br>		
+				<h4 style="text-aling: center;">Caso escolha cadastro ou atualização de produto complete os campos abaixo:</h4>	
 
-			<br><br>
+				<br>
+				<div class="form-group">
+					<input type="text" id="nome" class="form-control" required>
+					<label class="form-control-placeholder">Nome desejado:</label>
+				</div>
+				<br>
 
-			<!-- <label for="let d of products">{{d.nome}}</label> -->
-			
-		<!-- <button onClick="<script language='javascript'>window.location='admin';</script>">sdasda</button> -->
-			
-	</div>
+				<div class="form-group">
+					<input type="text" id="estoque" class="form-control" required>
+					<label class="form-control-placeholder">Quantidade em estoque:</label>
+				</div>
+				<br>
+
+				<div class="form-group">
+					<input type="text" id="preco" class="form-control" required>
+					<label class="form-control-placeholder">Preço:</label>
+				</div>
+				<br>
+
+				<button type="button" class="btn btn-primary btn-lg btn-block" onclick="enviar()">ENVIAR</button>
+				<br><br>
+				<ul id="repo-list"></ul>
+		</div>
 		
 </body>
+<style>
+	.form-group {
+		position: relative;
+		margin-bottom: 1.5rem;
+	}
+
+	.form-control-placeholder {
+		position: absolute;
+		top: 0;
+		padding: 7px 0 0 13px;
+		transition: all 200ms;
+		opacity: 0.5;
+	}
+
+	.form-control:focus + .form-control-placeholder,
+	.form-control:valid + .form-control-placeholder {
+		font-size: 120%;
+		transform: translate3d(0, -100%, 0);
+		opacity: 1;
+	}
+</style>
 </html>
